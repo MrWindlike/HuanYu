b0b5d33125efa487f47af7f9f01e653490e02290
Wind1ike <838596151@qq.com>
2017-05-06 09:46:43 +0800

    change main.js

--- main.js
+++ main.js
@@ -21,24 +21,40 @@ $(".carousel").carousel({num:5,path:"./dist/assets/img/"});
 
 var TEMPLATE = `<li><a href="/view.html?id={{_id}}"><i class="iconfont icon">&#xe60a;</i><span>{{title}}</span></a></li>`;
 
-
-$.ajax({
-	url : "http://www.guozewei.cn/title",
-	type: "GET",
-	dataType : "JSON",
-	success : function(data){
-		var lists = $(".lists");
-		console.log(data);
-		for(var i=0;i<data.length;i++){
-            var temp = TEMPLATE;
-            for(key in data[i]){
-            	if(key == "title")
-            		data[i][key] = data[i][key].trim();
-                temp = temp.replace("{{"+key+"}}",data[i][key]);
-            }
-            lists.append(temp);
-		}
-
-	}
-		
+GetTitle({type:"最新消息"},function(data){
+	var lists = $(".news");
+    lists.append(data);
 });
+
+GetTitle({type:"近期开班"},function(data){
+    var lists = $(".course");
+    lists.append(data);
+});
+
+GetTitle({type:"近期活动"},function(data){
+    var lists = $(".active");
+    lists.append(data);
+});
+
+var lists = $(".lists");
+
+
+function GetTitle(data,callback){
+    $.ajax({
+        url : "http://www.guozewei.cn/title",
+        type: "POST",
+        data:data,
+        success : function(data){
+        	console.log(data);
+            for(var i=0;i<data.length;i++){
+                var temp = TEMPLATE;
+                for(key in data[i]){
+                    if(key == "title")
+                        data[i][key] = data[i][key].trim();
+                    temp = temp.replace("{{"+key+"}}",data[i][key]);
+                }
+				callback(temp);
+            }
+        }
+    });
+}
